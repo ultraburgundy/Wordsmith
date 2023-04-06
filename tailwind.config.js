b6ff594081420ea764f94298cb55951c1bcd5171
rem <ultraburgundy@gmail.com>
2023-04-05 23:18:21 -0400

    is this the one?

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import("tailwindcss").Config} */
 module.exports = {
-  content: ["./public/index.html"],
+  content: ["./dist/*.html"],
   theme: {
     fontFamily: {
       "cutive": ["Cutive Mono"],
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "index[contenthash].js",
+        clean: true,
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+        assetModuleFilename: "[name][ext]",
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [ "style-loader", "css-loader", "postcss-loader",],
+            },
+            {
+                test: /\.(png|gif|svg|jpg)$/i,
+                type: "asset/resource"
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+           
+              },
+        ],
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            title: "Wordsmith",
+            filename: "index.html",
+            template: "./src/index.html",
+        })
+    ]
+}
